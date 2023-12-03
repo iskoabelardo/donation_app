85a43331fb4ba0898d83b952ee00e3ce7dd4d6cf
iskoabelardo <qafgabelardo@tip.edu.ph>
2023-12-04 01:13:23 +0800

    profiles fully working

--- tabs/OrgProfile.js
+++ tabs/OrgProfile.js
@@ -1,35 +1,68 @@
 import { View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { ChevronLeftIcon } from 'react-native-heroicons/solid';
 import { BellIcon } from 'react-native-heroicons/outline'
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function OrgProfile() {
 
-    const orgDetails = [
-        {
-          label: 'NGO Name:',
-          value: 'Philippine Red Cross',
-          key: '1',
-        },
-        {
-          label: 'Email:',
-          value: 'phredcross@gmail.com',
-          key: '2',
-        },
-        {
-          label: 'Mobile Number:',
-          value: '09516531345',
-          key: '3',
-        },
-        {
-          label: 'Location:',
-          value: 'Quezon City',
-          key: '4',
-        },
-      ];
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [location, setLocation] = useState('')
 
+  
+
+  var orgDetails = [
+    {
+      label: 'NGO Name:',
+      value: name,
+      key: '1',
+    },
+    {
+      label: 'Email:',
+      value: email,
+      key: '2',
+    },
+    {
+      label: 'Mobile Number:',
+      value: number,
+      key: '3',
+    },
+    {
+      label: 'Location:',
+      value: location,
+      key: '4',
+    },
+  ];
+
+  useFocusEffect(
+      React.useCallback(() => {
+          const fetchData = async () => {
+          try {
+              // Your asynchronous code goes here
+              const user = await AsyncStorage.getItem('user-session');
+              const userData = JSON.parse(user);
+              console.log(userData)
+              
+              if (userData !== null){
+                setName(userData.name);
+                setEmail(userData.email);
+                setNumber(userData.number);
+                setLocation(userData.location);
+              }
+          } catch (error) {
+              console.error('Error fetching data:', error);
+          }
+          };
+
+          fetchData();
+
+      }, [])
+  );
 
     return(
         <SafeAreaView className="flex-1 bg-gray" style={{backgroundColor: '#75BAA4'}}>
